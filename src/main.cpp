de5238fe5a4564db17fe674d504ecb02ca0484b1
Ismaele Giuseppe Recca <igrecca27@outlook.com>
2024-11-04 13:46:23 +0000

    Included JSON Parser Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <fstream>
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 int main()
 {
     std::cout << "Hello World" << std::endl;
+
+    std::fstream input("../inputs/test.json");
+    json data = json::parse(input);
+
+    std::cout << "The tested molecule is " << data["run"]["Molecule"] << std::endl;
 
     return 0;
 }
