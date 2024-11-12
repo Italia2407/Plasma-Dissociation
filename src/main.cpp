6408f68c52b8876b73dd78fc1d14ae901a9e4931
Ismaele Giuseppe Recca <igrecca27@outlook.com>
2024-11-12 13:51:00 +0000

    Implemented TOML File Parsing

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
+#include <unistd.h>
+#include <limits.h>
+
+#include <regex>
+
 int main(int argc, char *argv[])
 {
     // Create Command Line Options
@@ -34,10 +39,58 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    auto test = variableMap["input-files"].as<std::vector<std::string>>()[0];
-    std::string filePath = fmt::format("{0}/Molecule-Tests/{1}", ASSETS_PATH, variableMap["input-files"].as<std::vector<std::string>>()[0]);
-    auto testFile = toml::parse_file(filePath);
-    std::cout << "The tested molecule is " << testFile["Run"]["Molecule"].value_or("") << std::endl;
+    // Parse Inputted TOML File
+    toml::v3::ex::parse_result inputFile;
+    try {
+        std::string filePath = fmt::format("{0}/Molecule-Tests/{1}", ASSETS_PATH, variableMap["input-files"].as<std::vector<std::string>>()[0]);
+        inputFile = toml::parse_file(filePath);
+    }
+    catch(const toml::parse_error& error) {
+        std::cerr << error.what() << std::endl;
+        std::cerr << "ERROR Occurred" << std::endl;
+        return 0;
+    }
+
+    // Check Basic Arguments
+    bool argCheck = true;
+
+    if (inputFile["Setup"]["Repetitions"].type() != toml::v3::node_type::integer) {
+        std::cerr << "ERROR: Setup.Repetitions must be an Integer" << std::endl;
+        argCheck = false;
+    }
+    else if (inputFile["Setup"]["Repetitions"].value_or(0) < 1) {
+        std::cerr << "ERROR: Number of Repetitions must be at least 1" << std::endl;
+        argCheck = false;
+    }
+
+    if (inputFile["Setup"]["Cores"].type() != toml::v3::node_type::integer) {
+        std::cerr << "ERROR: Setup.Cores must be an Integer" << std::endl;
+        return 0;
+    }
+    else if (inputFile["Setup"]["Cores"].value_or(0) < 1) {
+        std::cerr << "ERROR: Number of Cores Selected must be at least 1" << std::endl;
+        argCheck = false;
+    }
+    else if (inputFile["Setup"]["Cores"].value_or(0) > MAX_CORES) {
+        std::cerr << fmt::format("ERROR: Maximum Number of Cores available is {0}", MAX_CORES) << std::endl;
+        argCheck = false;
+    }
+
+    if (inputFile["Run"]["GeomFLG"].type() != toml::v3::node_type::boolean) {
+        std::cerr << "ERROR: Run.GeomFLG must be a Boolean" << std::endl;
+        argCheck = false;
+    }
+
+    // 
+    if (!argCheck)
+        return 0;
+    std::cout << "Tested Molecule: " << inputFile["Run"]["Molecule"].value_or("") << std::endl;
+
+    // Check if Running on HPC Server
+    char hostName[HOST_NAME_MAX]; gethostname(hostName, HOST_NAME_MAX);
+    bool HPCFlag = std::regex_match(hostName, std::regex("login(1|2).arc(3|4).leeds.ac.uk"));
+
+    std::cout << hostName << std::endl;
 
     return 1;
 }
